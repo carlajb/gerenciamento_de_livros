4e0570dd927540c2a7717f43a23796ad1e534b20
carla <carla_agencia@hotmail.com>
2024-07-22 21:13:53 -0300

    types routes

---
+++ resources/views/jogos.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1> Testando Routes com views</h1>
+  {{-- <p>Id dojogo:{{$idJogo}}</p>
+  <p>Nome do jogo:{{ $nomeJogo }}</p> --}}
+
+  <a href="{{ route('home-index') }}"> Clique aqui</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::view('/jogos', 'jogos');
+
+// Route::get('/jogos', function(){
+//     return "Testando Rotas";
+// });
+
+// Route::view('/jogos', 'jogos', ['name' =>'GTA']);
+
+// Route::get('/jogos/{name?}', function($name = null){
+//     return view('jogos',['nomeJogo' => $name]);
+// })->where('name', '[A-Z-a-z]+');
+
+// Route::get('/jogos/{id?}', function($id = null){
+//     return view('jogos',['idJogo' => $id]);
+// })->where('id', '[0-9]+');
+
+// Route::get('/jogos/{id?}/{name?}', function($id = null, $name = null){
+//     return view('jogos',['idJogo' => $id, 'nomeJogo' => $name]);
+// })->where('id', '[0-9]+', 'name', '[A-Z-a-z]+');
+
+Route::get('/jogos', function(){
+    return view('jogos');
+});
+
+Route::get('/home', function () {
     return view('welcome');
+})->name('home-index');
+
+Route::fallback(function(){
+    return "Erro ao localizar a rota.";
 });
